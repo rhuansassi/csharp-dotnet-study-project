cc7b80dee337328bd371b0e786a78d1a3fd1fa3a
Rhuan Sassi <rhuansassi@gmail.com>
2025-01-18 20:10:11 -0300

    feat: adds Custom Exceptions

--- src/Shared/MyRecipeBook.Exceptions/ResourceMessagesException.Designer.cs
+++ src/Shared/MyRecipeBook.Exceptions/ResourceMessagesException.Designer.cs
@@ -95,5 +95,14 @@ namespace MyRecipeBook.Exceptions {
                 return ResourceManager.GetString("PASSWORD_EMPTY", resourceCulture);
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Unknown error..
+        /// </summary>
+        public static string UNKNOWN_ERROR {
+            get {
+                return ResourceManager.GetString("UNKNOWN_ERROR", resourceCulture);
+            }
+        }
     }
 }
